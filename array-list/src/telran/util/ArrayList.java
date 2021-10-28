da3f5975c8290054effb93d959725451cf5f71d7
NTsoi <czoj.n95@mail.ru>
2021-10-28 18:06:28 +0300

    adds CW #7

--- array-list/src/telran/util/ArrayList.java
+++ array-list/src/telran/util/ArrayList.java
@@ -1,6 +1,7 @@
 package telran.util;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.Predicate;
 
 public class ArrayList<T> implements List<T> {
@@ -24,7 +25,8 @@ public class ArrayList<T> implements List<T> {
 		
 		
 	}
-
+	
+		
 	private void allocate() {
 		array = Arrays.copyOf(array, array.length * 2);
 		
@@ -50,6 +52,7 @@ public class ArrayList<T> implements List<T> {
 
 	@Override
 	public int size() {
+		
 		return size;
 	}
 
@@ -65,93 +68,59 @@ public class ArrayList<T> implements List<T> {
 	}
 	@Override
 	public T remove(int index) {
-		T tmp = array[index];
-		if(!isValidIndex(index)) {
-			return null;
-		}
-		System.arraycopy(array, index+1, array, index, size-index-1);
-		size--;
-		return tmp;
-	}
-	@Override
-	public boolean contains(T pattern) {
-		boolean res = false;
-		for(int i = 0; i < size; i++) {
-			if (array[i].equals(pattern)) {
-				res = true;
-				break;
-			}
-		}
-		return res;
-	}
-	@Override
-	public int indexOf(T pattern) {
-		int res = -1;
-		for(int i = 0; i < size; i++) {
-			if (array[i].equals(pattern)) {
-				res = i;
-				break;
-			}
-		}
-		return res;
-	}
-	@Override
-	public int LastIndexOf(T pattern) {
-		int res = -1;
-		for(int i =size-1; i >=0; i--) {
-			if (array[i].equals(pattern)) {
-				res = i;
-				break;
-			}
-			
+		T res = null;
+		if (isValidIndex(index)) {
+			res = array[index];
+			size--;
+			System.arraycopy(array, index + 1, array, index, size - index);
+			//FIXME regarding setting null
 		}
 		
 		return res;
 	}
-	@Override
-	public boolean contains(Predicate<T> predicate) {
-		boolean res = false;
-		for(int i = 0; i < size; i++) {
-			if (predicate.test(array[i])) {
-				res = true;
-				break;
-			}
-		}
-		return res;
-	}
+	
+	
+	
+	
 	@Override
 	public int indexOf(Predicate<T> predicate) {
-		int res = -1;
-		for(int i = 0; i < size; i++) {
-			if (predicate.test(array[i])) {
-				res = i;
-				
-				break;
-			}
-		}
-		return res;
+				int res = -1;
+				for (int i = 0; i < size; i++) {
+					if (predicate.test(array[i])) {
+						res = i;
+						break;
+					}
+				}
+				return res;
 	}
 	@Override
-	public int LastIndexOf(Predicate<T> predicate) {
-		int res = -1;
-		for(int i = size-1; i >= 0; i--) {
-			if (predicate.test(array[i])) {
-				res = i;
-				break;
-			}
-		}
-		return res;
+	public int lastIndexOf(Predicate<T> predicate) {
+		
+				int res = -1;
+				for (int i = size - 1; i >=0 ; i--) {
+					if (predicate.test(array[i])) {
+						res = i;
+						break;
+					}
+				}
+				return res;
 	}
 	@Override
 	public boolean removeIf(Predicate<T> predicate) {
-		boolean res = false;
-		for(int i = size-1; i >=0; i--) {
+		int oldSize = size;
+		for (int i = size - 1; i >= 0; i--) {
 			if (predicate.test(array[i])) {
-				res = true;
 				remove(i);
 			}
 		}
-		return res;
+		
+		return oldSize > size;
 	}
+	@Override
+	public void sort(Comparator<T> comp) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
